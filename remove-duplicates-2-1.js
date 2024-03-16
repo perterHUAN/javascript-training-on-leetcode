b73fcfcf2e1479f1dc5e091d47b578b46e052bad
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-16 08:02:38 +0800

    Add solution

---
+++ remove-duplicates-2-1.js
@@ -0,0 +1,31 @@
+function removeDuplicates(nums) {
+  /*
+    [0, notSameIdx) 
+    [notSameIdx, nextToCheckIdx) empty
+    [nextToCheckIdx, n)
+    
+    current idx = nextToCheckIdx
+    pre
+    cnt
+  */
+  
+  let pre = null, cnt = 0;
+  let notSameIdx = 0, nextToCheckIdx = 0;
+  
+  while(nextToCheckIdx < nums.length) {
+    const val = nums[nextToCheckIdx++];
+    if(val !== pre) {
+      nums[notSameIdx++] = val;
+      pre = val;
+      cnt = 1;
+    }else {
+      if(cnt >= 2) {
+        ++cnt;
+      }else {
+        nums[notSameIdx++] = val;
+        ++cnt;
+      }
+    }
+  }
+  return notSameIdx;
+}
