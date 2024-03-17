132ffbc619186e6a77b54077683281228dd8082b
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-17 08:15:48 +0800

    Add solution

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Majority Element
 [addr](https://leetcode.cn/problems/majority-element/submissions/512857454/?envType=study-plan-v2&envId=top-interview-150)
 
+## Solution 2 Bit
+
---
+++ majority-element-2.js
@@ -0,0 +1,18 @@
+function majorityElement(nums) {
+  // bit
+  let res = 0;
+  for(let i = 0; i < 32; ++i) {
+    let OneCnt = 0, ZeroCnt = 0;
+    for(const e of nums) {
+      if(e & (1<<i)) {
+        OneCnt++;
+      }else {
+        ZeroCnt++;
+      }
+    }
+    if(OneCnt > ZeroCnt) {
+      res = res | (1<<i);
+    }
+  }
+  return res;
+}
