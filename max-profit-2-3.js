4570ed18f041e3db37cf62e5e1ec2ed36ae34b6a
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-21 09:04:00 +0800

    Add solution

---
+++ max-profit-2-3.js
@@ -0,0 +1,21 @@
+function maxProfit(prices) {
+  /*
+    dp[i][0],maxprofit after [0...i] transaction, finally don't have stock
+    dp[i][1],maxprofit after [0...i] transaction, finally have stock
+    
+    dp[i][0] = max dp[i-1][0], dp[i-1][1] + prices[i]
+    dp[i][1] = max dp[i-1][1], dp[i-1][0] - prices[i]
+    
+    dp[0][0] = 0
+    dp[0][1] = -prices[i];
+    
+    dp[i][0] > dp[i][1]
+  */
+  if(prices.length === 0) return 0;
+  let dp0 = 0, dp1 = -prices[0];
+  for(let i = 1; i < prices.length; ++i) {
+    dp0 = Math.max(dp0, dp1 + prices[i]);
+    dp1 = Math.max(dp1, dp0 - prices[i]);
+  }
+  return dp0;
+}
