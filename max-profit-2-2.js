dd8025262369cb89f56bb076bffd1293053b65d2
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-21 08:42:15 +0800

    Update comment

--- max-profit-2-2.js
+++ max-profit-2-2.js
@@ -3,6 +3,17 @@ function maxProfit(prices) {
    * A[i,j] = A[i+1] - A[i] + ... + A[j-1] - A[j]
    * max-profit = from(idx: 1..x) A[i_idx][j_idx], i_idx < j_idx, j_idx <= i_(idx+1)
    * 
+   * when to buy , when to sell?
+   * 	buy	sell
+   * 	inc	inc   	same line
+   * 	inc 	inc 	not same line
+   * 	inc 	des
+   * 	des	des
+   * 	des	inc
+   *
+   * expect method 1, we can find a method to get more profit on other
+   * method by method 1.
+   * 	
    */
   // if current day's price > yester day's price, buy on yesterday and sell current day.
   let res = 0;
