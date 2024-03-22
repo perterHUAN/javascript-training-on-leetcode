4dc67c595583f0b29944c2650bc47f3c39d3f089
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-22 10:05:25 +0800

    improve performance by preprocessing arrays and leverageing the state from the last loop iteration

--- can-jump-1.js
+++ can-jump-1.js
@@ -8,20 +8,27 @@ function canJump(nums) {
     next to calculate?
     [j..n)
   */
-  let currIdx = 0;
-  while (currIdx < nums.length - 1) {
+  /*
+    optimize:
+      1. Preprocessing to calculate the maximum reachable distance for each position
+      2. Using the state from the end of the previous cycle
+  */
+  const maxDistanceArray = nums.map((e,idx) => e + idx);
+  let currIdx = 0, nextToCheckIdx = currIdx + 1;
+  while(currIdx < nums.length - 1 && nextToCheckIdx < nums.length) {
     let r = nums[currIdx] + currIdx;
-    let nextIdx = -1,
-      maxDistance = -1;
-    for (let i = currIdx + 1; i < nums.length && i <= r; ++i) {
-      const distance = nums[i] + i;
-      if (distance > maxDistance) {
+    let nextIdx = -1, maxDistance = -1;
+    while(nextToCheckIdx < nums.length && nextToCheckIdx <= r) {
+      const distance = maxDistanceArray[nextToCheckIdx];
+      if(distance > maxDistance) {
         maxDistance = distance;
-        nextIdx = i;
+        nextIdx = nextToCheckIdx;
       }
+      ++nextToCheckIdx;
     }
-    if (nextIdx === -1) return false;
+    if(nextIdx === -1) return false;
     currIdx = nextIdx;
+    nextToCheckIdx = Math.max(nextToCheckIdx, currIdx + 1);
   }
   return true;
 }
