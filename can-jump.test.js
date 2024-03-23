fe64a551b841cc78f487436935a6e7813fd44c4f
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-23 08:11:57 +0800

    Update test

--- can-jump.test.js
+++ can-jump.test.js
@@ -9,10 +9,12 @@ describe("canJump", function () {
     const nums1 = [3, 2, 1, 1, 1];
     assert.equal(canJump(nums1), true);
 
+    // 3 x
+    // 2 2
     const nums2 = [3, 2, 3, 0, 1];
     assert.equal(canJump(nums2), true);
 
-    const nums3 = [3, 2, 1, 1, 1];
+    const nums3 = [1, 2, 0, 1, 0];
     assert.equal(canJump(nums3), true);
 
     const nums4 = [3, 2, 1, 1, 0];
@@ -23,6 +25,9 @@ describe("canJump", function () {
 
     const nums6 = [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0];
     assert.equal(canJump(nums6), true);
+
+    const nums7 = [0];
+    assert.equal(canJump(nums7), true);
   });
 
   it("can't reach to last location", function () {
