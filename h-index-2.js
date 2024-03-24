2f6a1beb19bb70c1f786f23759c99e84934c2e8f
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-24 09:39:21 +0800

    per: using a differential array to quickly record the number added in a given prefix interval

--- h-index-2.js
+++ h-index-2.js
@@ -10,16 +10,41 @@ function hIndex(citations) {
     */
   let maxCitationCount =
     citations.reduce((pre, cur) => (pre > cur ? pre : cur)) + 1;
+  let diff = Array(maxCitationCount + 1).fill(0);
   let cnt = Array(maxCitationCount).fill(0);
   for (const e of citations) {
-    // how to make cnt[i] i >= e all increase 1 ? It's a hard question.
-    for (let i = 0; i <= e; ++i) {
-      cnt[i]++;
-    }
+    /* how to make cnt[i] i <= e all increase 1 ? It's a hard question.
+       difference equal
+       cnt[0] cnt[1] ... cnt[k] ... cnt[n]
+       cnt[0]+1 cnt[1]+1 ... cnt[k]+1 cnt[k] ... cnt[n]
+       diff[0] = cnt[0]
+       diff[1] = cnt[1] - cnt[0]
+       diff[2] = cnt[2] - cnt[1]
+       ...
+       diff[k] = cnt[k+1] - cnt[k]
+       ...
+
+       only diff[k] is changed after increasing.
+       diff[k]--;
+
+       how to calculate from diff arrayto cnt arary ?
+       cnt[0] = diff[0]
+       cnt[1] = cnt[0] + diff[1]
+       cnt[2] = cnt[1] + diff[2]
+       
+    */
+    diff[e + 1]--;
+    diff[0]++;
   }
-  let i;
-  for (i = maxCitationCount - 1; i >= 0 && cnt[i] < i; --i);
-  return i;
+  // revert to cnt
+  cnt[0] = diff[0];
+  for (let i = 1; i < maxCitationCount; ++i) {
+    cnt[i] = cnt[i - 1] + diff[i];
+  }
+  console.log("cnt: ", cnt);
+  let h;
+  for (h = maxCitationCount - 1; h >= 0 && cnt[h] < h; --h);
+  return h;
 }
 
 module.exports = hIndex;
