14d8c148de6c63d4eb766b0bd763649749beb6d1
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-28 08:03:00 +0800

    perf: replace precomputed products with real-time variable evaluation and use the output array to store intermediate results

--- product-expect-self-1.js
+++ product-expect-self-1.js
@@ -8,20 +8,21 @@ function productExceptSelf(nums) {
     */
   const n = nums.length;
   if (n === 0) return [];
-  const pre = Array(n).fill(1);
-  const suffix = Array(n).fill(1);
+  // const pre = Array(n).fill(1);
+  // const suffix = Array(n).fill(1);
   const res = Array(n).fill(1);
 
-  for (let i = 1; i < n; ++i) pre[i] = pre[i - 1] * nums[i - 1];
-  for (let i = n - 2; i >= 0; --i) suffix[i] = suffix[i + 1] * nums[i + 1];
+  // for (let i = 1; i < n; ++i) pre[i] = pre[i - 1] * nums[i - 1];
+  for (let i = n - 2; i >= 0; --i) res[i] = res[i + 1] * nums[i + 1];
   //      1 2 3
   // pre: 1 1 2
   // suf: 6 3 1
   // console.log(pre);
   // console.log(suffix);
-
+  let pre = 1;
   for (let i = 0; i < n; ++i) {
-    res[i] = pre[i] * suffix[i];
+    res[i] = pre * res[i];
+    pre *= nums[i];
   }
   return res;
 }
